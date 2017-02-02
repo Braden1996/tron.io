86848261f294b0f805ad92a74796249ae9409d07
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-02 13:02:11 +0000

    Add game folder and example of a JS import.

---
+++ src/site/assets/js/game/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+export default function gameMain() {
+	console.log("Test!");
+}
--- src/site/assets/js/main.js
+++ src/site/assets/js/main.js
@@ -1,3 +1,5 @@
-let evens = [0,2,4,6,8]
-let odds = evens.map(v => v + 1);
-console.log(odds);
+"use strict";
+
+import gameMain from "./game/main.js";
+
+gameMain();
