4429e43b08678050a1e3e2696afda42b74094aa2
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-03 23:37:48 +0000

    Add simple game loop.

--- src/site/assets/js/game/main.js
+++ src/site/assets/js/game/main.js
@@ -1,5 +1,28 @@
 "use strict";
 
+let lastTick = 0;
+
+function update(state, progress) {
+	console.log("UPDATE!")
+}
+
+function draw(state) {
+	console.log("DRAW!");
+}
+
+function tick(state, curtick) {
+	let progress = curtick - lastTick;
+
+	update(progress);
+	draw();
+
+	lastTick = curtick - lastTick;
+	window.requestAnimationFrame((curtick) => tick(state, curtick));
+}
+
 export default function gameMain() {
-	console.log("Test!");
+	console.log("Starting!");
+
+	let state = 0;
+	window.requestAnimationFrame((curtick) => tick(state, curtick));
 }
