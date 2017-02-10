53cd731f83aaaa4cf1166a10d897e700b79b163c
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-10 20:14:54 +0000

    Add more player starting positions.

--- src/site/assets/js/game/state/game.js
+++ src/site/assets/js/game/state/game.js
@@ -4,12 +4,14 @@
 export default function createGameState(config) {
 	let state = {};
 
-	let gridMiddleX = Math.floor(config.arenaSize / 2);
+	let gridMiddle = Math.floor(config.arenaSize / 2);
 
 	// Create default state for each of the intended players.
 	state.players = [
-		{"position": [gridMiddleX, config.arenaSize], "direction": "north", "trail": [], "alive": true},
-		//{"position": [gridMiddleX, 0], "direction": "south", "trail": [], "alive": true}
+		{"position": [gridMiddle, config.arenaSize], "direction": "north", "trail": [], "alive": true},
+		//{"position": [gridMiddle, 0], "direction": "south", "trail": [], "alive": true},
+		//{"position": [0, gridMiddle], "direction": "east", "trail": [], "alive": true},
+		//{"position": [config.arenaSize, gridMiddle], "direction": "west", "trail": [], "alive": true}
 	];
 
 	// Add clone of start position to trail array.
