ea616d5a5a9f2799fd288ec34b1083d52d87e007
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-11 00:25:15 +0000

    Progress made towards trail collision.

--- src/site/assets/js/game/update/collision.js
+++ src/site/assets/js/game/update/collision.js
@@ -15,6 +15,63 @@ function collideBorder(state, ply) {
 }
 
 function collideTrail(state, ply) {
+	let plyX = ply.position[0];
+	let plyY = ply.position[1];
+
+	for (let ply2 of state.game.players) {
+		let lastX = ply2.position[0];
+		let lastY = ply2.position[1];
+
+		// Ignore the current line of our own player.
+		let trailLength = ply2.trail.length;
+		if (ply === ply2) {
+			trailLength -= 1;
+			lastX = ply2.trail[trailLength][0];
+			lastY = ply2.trail[trailLength][1];
+		}
+
+		for (let i = trailLength; i-- > 0; ) {
+			let trailX = ply2.trail[i][0];
+			let trailY = ply2.trail[i][1];
+
+			// If line is north-south/south-north.
+			if (trailX - lastX === 0) {
+				let distance = plyX - trailX;
+				let minY = Math.min(trailY, lastY);
+				let maxY = Math.max(trailY, lastY);
+				if (Math.abs(distance) < 1 && plyY > minY && plyY < maxY) {
+					if (distance === 0) {
+						if (Math.abs(plyY - minY) < Math.abs(plyY - maxY)) {
+							return [plyX, minY + 1];
+						} else {
+							return [plyX, maxY + 1];
+						}
+					} else {
+						return [trailX + (distance > 0 ? 1 : -1), plyY];
+					}
+				}
+
+			// Else, if line is west-east/east-west.
+			} else {
+				let distance = plyY - trailY;
+				let minX = Math.min(trailX, lastX);
+				let maxX = Math.max(trailX, lastX);
+				if (Math.abs(distance) < 1 && plyX > minX && plyX < maxX) {
+					if (distance === 0) {
+						if (Math.abs(plyX - minX) < Math.abs(plyX - maxX)) {
+							return [minX - 1, plyY];
+						} else {
+							return [maxX + 1, plyY];
+						}
+					} else {
+						return [plyX, lastY + (distance > 0 ? 1 : -1)];
+					}
+				}
+			}
+			lastX = trailX;
+			lastY = trailY;
+		}
+	}
 	return undefined;
 }
 
