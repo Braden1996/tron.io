70271da2fd38c3fbb6cc5684f506e80cd19eacc5
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-16 21:56:24 +0000

    Fix input so that it is more compatible with the new menu.

--- src/site/assets/js/game/update/control.js
+++ src/site/assets/js/game/update/control.js
@@ -6,32 +6,35 @@ export default function updateControl(state, progress) {
 		let ply = state.game.players[0];
 
 		if (ply.alive) {
-			// Player must move at least one grid cell before changing direction.
-			let lastPoint = ply.trail.slice(-1)[0];
+			let newDirection;
+			if (state.input.pressedKeys["w"]) {
+				newDirection = "north";
+			} else if (state.input.pressedKeys["a"]) {
+				newDirection = "west";
+			} else if (state.input.pressedKeys["s"]) {
+				newDirection = "south";
+			} else if (state.input.pressedKeys["d"]) {
+				newDirection = "east";
+			}
 
-			// No need for Pythagoras, as we can only move along one axis. 
-			let curDistance = Math.abs(lastPoint[0] - ply.position[0] + lastPoint[1] - ply.position[1]);
-			
-			if (curDistance >= 1) {
-				let oldDirection = ply.direction;
-				let newDirection;
-				if (state.input.pressedKeys["w"]) {
-					newDirection = "north";
-				} else if (state.input.pressedKeys["a"]) {
-					newDirection = "west";
-				} else if (state.input.pressedKeys["s"]) {
-					newDirection = "south";
-				} else if (state.input.pressedKeys["d"]) {
-					newDirection = "east";
-				}
+			if (newDirection !== undefined) {
+				// Player must move at least one grid cell before changing direction.
+				let lastPoint = ply.trail.slice(-1)[0];
 
-				if (newDirection !== oldDirection &&
-					!(oldDirection === "north" && newDirection === "south") &&
-					!(oldDirection === "south" && newDirection === "north") &&
-					!(oldDirection === "west" && newDirection === "east") &&
-					!(oldDirection === "east" && newDirection === "west")) {
-					ply.direction = newDirection;
-					ply.trail.push(ply.position.slice());
+				// No need for Pythagoras, as we can only move along one axis. 
+				let curDistance = Math.abs(lastPoint[0] - ply.position[0] + lastPoint[1] - ply.position[1]);
+				
+				if (curDistance >= 1) {
+					let oldDirection = ply.direction;
+
+					if (newDirection !== oldDirection &&
+						!(oldDirection === "north" && newDirection === "south") &&
+						!(oldDirection === "south" && newDirection === "north") &&
+						!(oldDirection === "west" && newDirection === "east") &&
+						!(oldDirection === "east" && newDirection === "west")) {
+						ply.direction = newDirection;
+						ply.trail.push(ply.position.slice());
+					}
 				}
 			}
 		}
