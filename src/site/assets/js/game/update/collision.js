d44044d8ad48d77c4e2beac66e48bddf0e36d323
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-19 20:27:09 +0000

    Reimplement player trail collision (again). Now converts positions/trail to rectangles.

--- src/site/assets/js/game/update/collision.js
+++ src/site/assets/js/game/update/collision.js
@@ -14,73 +14,50 @@ function collideBorder(state, ply) {
 	}
 }
 
+function lineToRect(x0, y0, x1, y1, expand) {
+	return {
+		x: Math.min(x0, x1) - expand/2,
+		y: Math.min(y0, y1) - expand/2,
+		w: Math.abs(x0 - x1) + expand,
+		h: Math.abs(y0 - y1) + expand
+	};
+}
+
 function collideTrail(state, ply) {
-	// Line segment created at the current tick.
+	let plySize = 1;
+
+	// Convert ply's line-segment from this tick into a rectangle.
 	let plyX0 = ply.position[0];
 	let plyY0 = ply.position[1];
 	let plyX1 = ply.trail[ply.trail.length-1][0];
 	let plyY1 = ply.trail[ply.trail.length-1][1];
 
-	// Reduce function that will return the point closest to the
-	// start point of ply's current line segment.
-	let distanceCheck = (pre, cur) => {
-		let preDist = Math.hypot(pre[0] - plyX1, pre[1] - plyY1);
-		let curDist = Math.hypot(cur[0] - plyX1, cur[1] - plyY1);
-		return preDist < curDist ? pre : cur;
-	};
+	let plyRect = lineToRect(plyX0, plyY0, plyX1, plyY1, plySize);
 
 	// For now, we check for collisions against every trail in the arena.
 	// There may be room for optimisation here...
 	for (let ply2 of state.game.players) {
 		// Check backwards from each players' current position.
+		// But ignore plys most recent trail progress.
 		let ply2X0 = ply2.position[0];
 		let ply2Y0 = ply2.position[1];
 
-		let trailLength = ply2.trail.length;
-
-		// Ignore ply's most recent trail.
-		if (ply === ply2) {
-			trailLength -= 1;
-			ply2X0 = ply2.trail[trailLength][0];
-			ply2Y0 = ply2.trail[trailLength][1];
-		}
-
-		for (let i = trailLength; i-- > 0; ) {
+		for (let i = ply2.trail.length - 1; i >= 0; i--) {
 			let ply2X1 = ply2.trail[i][0];
 			let ply2Y1 = ply2.trail[i][1];
 
-			// If line is north-south/south-north.
-			if (ply2X0 === ply2X1) {
-				let ply2MinY = Math.min(ply2Y0, ply2Y1);
-				let ply2MaxY = ply2MinY === ply2Y0 ? ply2Y1 : ply2Y0;
-				if (plyY0 > ply2MinY && plyY0 < ply2MaxY && (plyX0 === ply2X0 && plyX1 === ply2X0 ||
-					Math.sign(plyX0 - ply2X0) !== Math.sign(plyX1 - ply2X0))) {
-					let hitPoints = [
-						[plyX0, ply2MinY - 1],
-						[plyX0, ply2MaxY + 1],
-						[ply2X0 + 1, plyY0],
-						[ply2X0 - 1, plyY0]
-					];
+			// Ignore ply's two most-recent trail line-segments.
+			if (ply !== ply2 || i < ply.trail.length - 3) {
+				let ply2Rect = lineToRect(ply2X0, ply2Y0, ply2X1, ply2Y1, plySize);
 
-					return hitPoints.reduce(distanceCheck);
-				}
-
-			// Else, if line is west-east/east-west.
-			} else {
-				let ply2MinX = Math.min(ply2X0, ply2X1);
-				let ply2MaxX = ply2MinX === ply2X0 ? ply2X1 : ply2X0;
-				if (plyX0 > ply2MinX && plyX0 < ply2MaxX && (plyY0 === ply2Y0 && plyY1 === ply2Y0 ||
-					Math.sign(plyY0 - ply2Y0) !== Math.sign(plyY1 - ply2Y0))) {
-					let hitPoints = [
-						[ply2MinX - 1, plyY0],
-						[ply2MaxX + 1, plyY0],
-						[plyX0, ply2Y0 - 1],
-						[plyX0, ply2Y0 + 1]
-					];
-
-					return hitPoints.reduce(distanceCheck);
+				if (plyRect.x < ply2Rect.x + ply2Rect.w &&
+			        plyRect.x + plyRect.w > ply2Rect.x &&
+			        plyRect.y < ply2Rect.y + ply2Rect.h &&
+			        plyRect.h + plyRect.y > ply2Rect.y) {
+					return [plyX0, plyY0];
 				}
 			}
+
 			ply2X0 = ply2X1;
 			ply2Y0 = ply2Y1;
 		}
