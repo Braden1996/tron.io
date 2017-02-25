969ba38f92e12134b4fe9e5f2c76362452951fc2
Braden Marshall <braden1996@hotmail.co.uk>
2017-02-25 18:11:59 +0000

    Fix game finish condition for a single player.

--- src/site/assets/js/game/update/index.js
+++ src/site/assets/js/game/update/index.js
@@ -15,8 +15,8 @@ export default function update(store, progress) {
 		updateMove(store, progress);
 		updateCollision(store, progress);
 
-		if ((state.players.size === 1 && state.players.get(0).get("alive")) ||
-			(state.players.count(p => p.get("alive")) <= 1)) {
+		if ((state.players.size === 1 && !state.players.get(0).get("alive")) ||
+			(state.players.size !== 1 && state.players.count(p => p.get("alive")) <= 1)) {
 			store.dispatch(updateFinishGame(true));
 		}
 	}
