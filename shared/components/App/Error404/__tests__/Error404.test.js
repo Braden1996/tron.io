56fae83186a1879dc1b0215b615f75124e49f46b
Braden Marshall <braden1996@hotmail.co.uk>
2017-03-17 15:11:23 +0000

    Forgot to update Error404 to the latest React universally version following the recent update.

--- shared/components/App/Error404/__tests__/Error404.test.js
+++ shared/components/App/Error404/__tests__/Error404.test.js
@@ -2,11 +2,14 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import Error404 from './Error404';
+
+import Error404 from '../index';
 
 describe('<Error404 />', () => {
   test('renders', () => {
-    const wrapper = shallow(<Error404 />);
+    const staticContext = {};
+    const wrapper = shallow(<Error404 staticContext={staticContext} />);
     expect(wrapper).toMatchSnapshot();
+    expect(staticContext.missed).toBeTruthy();
   });
 });
